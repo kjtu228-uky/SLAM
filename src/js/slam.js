b4e9c9de336ac41028ca036cc17f37eb336fade6
Tuck <kjtu228@uky.edu>
2025-11-04 10:28:18 -0500

    Create slam.js
    Script for making calls to update tools.

---
+++ src/js/slam.js
@@ -0,0 +1,60 @@
+async function updateToolInstall(tool_id, course_number) {
+	tool_toggle = document.getElementById("tool_select_" + tool_id);
+	update_path = window.location.href.substring(0, document.location.href.lastIndexOf("/")) + '/update_tool.php?';
+	update_path = update_path + 'tool_id=' + tool_id + '&action=';
+	action = tool_toggle.checked ? 'add' : 'remove';
+	update_path += action;
+	tool_toggle.disabled = true;
+
+	if (document.getElementById("tool_message_" + tool_id) != null) {
+		document.getElementById("tool_message_" + tool_id).style.display = "none";
+	}
+	tool_toggle.disabled = false;
+	fetch(update_path, {
+			method: "GET",
+			mode: "no-cors"
+		})
+		.then(response => response.json()).then(data => {
+			updateToggles(data);
+			if (action == 'add' && document.getElementById("tool_message_" + tool_id) != null) {
+				message_box = document.getElementById("tool_message_text_" + tool_id);
+				message_box.innerHTML = message_box.innerHTML.replaceAll('\[DEPLOYMENT_ID\]', data[tool_id]['deployment_id']);
+				message_box.innerHTML = message_box.innerHTML.replaceAll('\[TOOL_NAME\]', data[tool_id]['name']);
+				document.getElementById("tool_message_" + tool_id).style.top = (tool_toggle.getBoundingClientRect().top - document.body.getBoundingClientRect().top) + "px";
+				document.getElementById("tool_message_" + tool_id).style.display = "block";
+			}
+		}).catch(error => {
+			console.log(error);
+		});
+}
+
+function toolNoticeResponse(tool_id, cancelAdd) {
+	if (document.getElementById("tool_message_" + tool_id) != null)
+		document.getElementById("tool_message_" + tool_id).style.display = "none";
+	if (cancelAdd)
+		document.getElementById("tool_select_"+tool_id).click();
+}
+
+function updateToggles(toolsStatus) {
+ 	for (var key in toolsStatus) {
+		var toggle = 'tool_select_' + key;
+		var tool_container = 'lti_tool_' + key;
+		if (document.getElementById(toggle) != null) {
+			if (toolsStatus[key]['enabled'] > 0) {
+				document.getElementById(toggle).checked = true;
+				document.getElementById(tool_container).classList.add("lti-tool-enabled");
+			} else {
+				document.getElementById(toggle).checked = false;
+				document.getElementById(tool_container).classList.remove("lti-tool-enabled");
+			}
+		}
+	}
+}
+
+function setToolContainerSize() {
+	toolContainerHeight = parseInt(window.innerHeight) -
+		parseInt(document.getElementById('slamTitle').clientHeight) -
+		parseInt(document.getElementById('slamDescription').clientHeight) -
+		parseInt(document.getElementById('courseTitle').clientHeight) - 100;
+	document.getElementById('toolList').style.height = toolContainerHeight + "px";
+}
