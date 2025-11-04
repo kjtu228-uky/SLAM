d68f9f2bd8ce2c62362968cae5f54627bbf564f9
Tuck <kjtu228@uky.edu>
2025-11-04 10:26:20 -0500

    Create oauth2response.php
    Handle OAuth requests for Canvas tokens.

---
+++ src/oauth2response.php
@@ -0,0 +1,47 @@
+<?php
+// Don't do anything if no $_GET value is sent.
+require_once('../config.php');
+
+// Initialise session and database
+$db = null;
+$ok = init($db, true);
+
+if(count($_GET) > 0) {
+	if(isset($_GET['code']) && isset($_GET['state'])) {
+		$url = CANVAS_URL . '/login/oauth2/token';
+		$ch = curl_init();
+		curl_setopt ($ch, CURLOPT_URL, $url);
+		curl_setopt ($ch, CURLOPT_HEADER, true);
+		curl_setopt ($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded"));
+		curl_setopt ($ch, CURLOPT_POST, true);
+		curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+						'grant_type' => 'authorization_code',
+						'client_id' => CLIENT_ID,
+						'client_secret' => CLIENT_SECRET,
+						'redirect_uri' => APP_URL . 'oauth2response.php',
+						'code' => $_GET['code'],
+						'replace_tokens' => '1'
+						)));
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);	
+		$response = curl_exec($ch);
+		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($response, 0, $header_size);
+		// TO DO: check $header to make sure we're OK
+		//$for_session['headers'] = get_headers_from_curl_response($header);
+		$response_data = json_decode(substr($response, $header_size), true);
+		if (isset($response_data['expires_in'])) $response_data['refresh_at'] = time() + intval($response_data['expires_in']);
+		// get the platform associated with the state id
+		$dataConnector = DataConnector\DataConnector::getDataConnector($db, DB_TABLENAME_PREFIX);
+		$platform = Platform::fromRecordId($_GET['state'], $dataConnector);
+		// update the token values
+		$platform->setSetting('access_token', json_encode($response_data));
+		$platform->save();
+		// redirect
+		header('Location: ' . APP_URL . 'index.php');
+		exit(0);
+	} else if(isset($_GET['error'])) {
+		print('<p><strong>Error: </strong>' . $_GET['error']);
+		print('<p><strong>Description: </strong>' . $_GET['error_description']);
+	}
+}
+?>
