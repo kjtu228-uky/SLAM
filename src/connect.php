e8fdd3bb1e8d13d6d6e328889dc7662f06a81cee
Tuck <kjtu228@uky.edu>
2025-11-04 11:14:29 -0500

    Update connect.php
    Load the config.php file and ensure the Util class is loaded.

--- src/connect.php
+++ src/connect.php
@@ -2,6 +2,7 @@
 
 use ceLTIc\LTI\DataConnector;
 use ceLTIc\LTI\Platform;
+use ceLTIc\LTI\Util;
 use ceLTIc\LTI\Enum\LogLevel;
 
 /**
@@ -11,7 +12,7 @@ use ceLTIc\LTI\Enum\LogLevel;
  * @copyright  SPV Software Products
  * @license  http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
  */
-require_once('vendor/autoload.php');
+require_once('config.php');
 require_once('SLAM.php');
 
 // Cancel any existing session
