f170a36405cecdd5383e5e0f9241b3c329b523fe
Tuck <kjtu228@uky.edu>
2025-11-06 16:00:19 -0500

    Update jwks.php
    Does this really need db.php?

--- src/jwks.php
+++ src/jwks.php
@@ -9,7 +9,7 @@ use ceLTIc\LTI\Jwt\Jwt;
  * @copyright  SPV Software Products
  * @license  http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
  */
-require_once('db.php');
+//require_once('db.php');
 
 $jwt = Jwt::getJwtClient();
 $keys = $jwt::getJWKS(PRIVATE_KEY, SIGNATURE_METHOD, KID);
