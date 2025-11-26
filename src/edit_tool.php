83a6bec27520eb0c4a6bc73bd193ecf515f5e1fb
Tuck <kjtu228@uky.edu>
2025-11-26 10:39:08 -0500

    Create edit_tool.php
    Base code for the edit tool form. It is based off the tools_admin.php file to do checking that user is a tool admin.

---
+++ src/edit_tool.php
@@ -0,0 +1,53 @@
+<?php
+use ceLTIc\LTI;
+use ceLTIc\LTI\DataConnector;
+use ceLTIc\LTI\Platform;
+use ceLTIc\LTI\Util;
+use ceLTIc\LTI\Enum\LogLevel;
+
+require_once('lib.php');
+
+$ok = true;
+if (isset($_SESSION['error_message'])) $ok = false;
+
+// Initialise session and database
+if ($ok) {
+	$db = null;
+	$ok = init($db, true);
+	// Initialise parameters
+	$dataConnector = DataConnector\DataConnector::getDataConnector($db, DB_TABLENAME_PREFIX);
+	$platform = Platform::fromRecordId($_SESSION['consumer_pk'], $dataConnector);
+	if (!platformHasToken($platform)) $ok = false;
+}
+
+if (!$ok || !isToolAdmin($platform) || !isset($_GET['id'])) {
+	header('Location: ' . TOOL_BASE_URL . 'index.php');
+	exit(0);
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Self LTI App Management - Tool Configuration</title>
+	<meta name="description" content="An LTI app that allows Canvas users to self-manage LTI apps in their course." />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="css/slam.css">
+</head>
+<body>
+<div style='display: flex; flex-direction: column; height: 98vh;'>
+<?php
+$showVal = function($val) {
+	return $val;
+};
+$body = <<< EOD
+	<div class='slam-title'>
+		<h1><img src='{$showVal(TOOL_BASE_URL)}/images/icon50.png' alt='SLAM logo'>Self-Service LTI App Management</h1>
+	</div>
+	<div class='slam-description'>
+		The form to edit will follow this section. Tool id: {$_GET['id']}
+	</div>
+EOD;
+?>
+</div>
+</body>
+</html>
