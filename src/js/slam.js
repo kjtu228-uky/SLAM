40d4ac622a3b4145685d7694d50d1cc9b07b9462
Tuck <kjtu228@uky.edu>
2025-12-17 09:22:30 -0500

    Update slam.js
    Try to replace [TOOL_NAME] with the tool's name.

--- src/js/slam.js
+++ src/js/slam.js
@@ -24,8 +24,9 @@ async function getCourseTools() {
 				toolHTML += "><span class='slider round'></span></div><div><label for='tool_select_" +
 					data[key]['id'] + "' class='toggle-label'>" + data[key]['name'] + "</label></div>";
 				if ('support_info' in data[key] && data[key]['support_info'] !== null && data[key]['support_info'].length > 0) {
-//					preg_replace('/\[TOOL_NAME\]/', $lti_tool['name'], $lti_tool['support_info'])
-					toolHTML += "<div class='tool-support'>" + data[key]['support_info'] + "</div>";
+					toolHTML += "<div class='tool-support'>";
+					toolHTML += data[key]['support_info'].replaceAll('\[TOOL_NAME\]', data[key]['name']);
+					toolHTML += "</div>";
 				}
 				toolHTML += "</div>";
 			}
