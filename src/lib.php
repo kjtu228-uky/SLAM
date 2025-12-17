7e45ae368e48ec71feb37761abf6174ea76a6c98
Tuck <kjtu228@uky.edu>
2025-12-17 10:13:10 -0500

    Update lib.php
    Log the response to figure out why the exception is not being added.

--- src/lib.php
+++ src/lib.php
@@ -545,6 +545,7 @@ function addToolToCourse($platform, $tool_id, $courseNumber) {
 		$response_headers = substr($response, 0, $response_header_size);
 		$response_body = substr($response, $response_header_size);
 		curl_close($ch);
+Util::logError($response_body);
 		if ($response_http_code != 200) return false;
 		$controls = json_decode($response_body, true);
 		if (isset($controls['course_id']) && isset($controls['available']) && $controls['available'])
