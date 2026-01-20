518ea9937804c15384fb59655555b513df685c38
Tuck <kjtu228@uky.edu>
2026-01-20 16:22:23 -0500

    Customize config-dist.php for SLAM
    The configuration options were simply carried forward from the forked project. Update to reflect SLAM-specific values.

--- src/config-dist.php
+++ src/config-dist.php
@@ -4,10 +4,9 @@ use ceLTIc\LTI\Util;
 use ceLTIc\LTI\Enum\LogLevel;
 
 /**
- * This page contains the configuration settings for the application.
+ * Configuration settings for the application.
  *
- * @author  Stephen P Vickers <stephen@spvsoftwareproducts.com>
- * @copyright  SPV Software Products
+ * @author  Kyle Tuck <kjtu228@uky.edu>
  * @license  http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
  */
 require_once('vendor/autoload.php');
@@ -25,22 +24,22 @@ define('REQUEST_URI_PREFIX', '');
 ###
 ###  App specific settings
 ###
-define('TOOL_ID', 'lti13');
-define('SESSION_NAME', 'lti13');
-define('TOOL_BASE_URL', '');
+define('TOOL_ID', 'slam13');
+define('SESSION_NAME', 'slam13');
+define('TOOL_BASE_URL', ''); // e.g. https://your_domain.com/SLAM/
 define('TOOL_UUID', '<your_UUID>'); // Linux command: uuidgen
-define('APP_NAME', 'Basic LTI 1.3');
-define('APP_DESCRIPTION', 'An LTI 1.3 test app.');
-define('APP_VERSION', '0.1.0');
-define('APP_URL', 'https://github.com/kylejtuck/Basic-LTI-PHP/');
-define('VENDOR_CODE', 'kjt');
-define('VENDOR_NAME', 'Kyle J Tuck');
-define('VENDOR_DESCRIPTION', 'Independent developer');
-define('VENDOR_URL', 'https://github.com/kylejtuck');
-define('VENDOR_EMAIL', 'kylejtuck@gmail.com');
+define('APP_NAME', 'SLAM');
+define('APP_DESCRIPTION', 'Self-service LTI App Manager for Canvas.');
+define('APP_VERSION', '1.3.1');
+define('APP_URL', 'https://github.com/kjtu228-uky/SLAM');
+define('VENDOR_CODE', 'ukonline');
+define('VENDOR_NAME', 'UK Online');
+define('VENDOR_DESCRIPTION', 'University of Kentucky | UK Online');
+define('VENDOR_URL', 'https://online.uky.edu/');
+define('VENDOR_EMAIL', 'elearning@uky.edu');
 define('INSTRUCTOR_ONLY', true);
-define('DEFAULT_DISABLED', true);
-define('CUSTOM_FIELDS', array(
+define('DEFAULT_DISABLED', true); // change to false if you want this enabled in course nav by default
+define('CUSTOM_FIELDS', array( // you shouldn't need anything here unless you are customizing SLAM
 #	'COURSE_NUMBER'=>"Canvas.course.id",
 #	'COURSE_NAME'=>"Canvas.course.name",
 #	'COURSE_SIS_ID'=>"Canvas.course.sisSourceId",
@@ -60,7 +59,9 @@ define('DB_TABLENAME_PREFIX', '');
 ###  LTI 1.3 Security settings
 ###
 define('SIGNATURE_METHOD', 'RS256');
-define('KID', '');  // A random string to identify the key value
+define('KID', '');  // A random string to identify the key value; in Linux use `pwgen 16` and choose one of the results
+// For the PRIVATE_KEY, in Linux use `openssl genrsa -out custom.key`
+// Copy the contents of custom.key into the following
 define('PRIVATE_KEY', <<< EOD
 -----BEGIN RSA PRIVATE KEY-----
 Insert private key here
@@ -81,6 +82,6 @@ define('API_SCOPES', array('url:GET|/api/v1/accounts/:account_id/lti_registratio
 ###
 ###  Dynamic registration settings
 ###
-define('AUTO_ENABLE', false);
-define('ENABLE_FOR_DAYS', 0);
+define('AUTO_ENABLE', false); // if false, you must manually enable the platform in the admin page
+define('ENABLE_FOR_DAYS', 0); // if you want to automatically enable but for a limited time, set this value
 ?>
