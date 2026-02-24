1088adb08d062474f1244cf010796b3c5f4c1a22
Tuck <kjtu228@uky.edu>
2026-02-24 15:35:10 -0500

    Add viewport for responsive page

--- src/index.php
+++ src/index.php
@@ -48,6 +48,7 @@ $page = <<< EOD
 <head>
   <meta http-equiv="content-language" content="EN" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{$showVal(APP_NAME)}</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="css/slam.css">
