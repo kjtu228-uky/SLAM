e60b574f6fc4e8e5329bf4fac2e4e35aca3c8a61
Tuck <kjtu228@uky.edu>
2026-03-06 14:12:10 -0500

    Breadcrumbs

--- src/admin/tools_admin.php
+++ src/admin/tools_admin.php
@@ -92,8 +92,8 @@ $showVal = function($val) {
 $body = <<< EOD
 	<div class='slam-title'>
 		<h1><img src='{$tool_base_url}/images/icon50.png' alt='SLAM logo'>Self-Service LTI App Management</h1>
-		<a href='{$tool_base_url}/index.php'>SLAM</a>
 	</div>
+	<a href='{$tool_base_url}/index.php'>SLAM</a>
 	<div class='tool-admin-panel'>
 		<form action="tools_admin.php" method="get" id="update_platform_form">
 			<input type="hidden" name="update_platform_settings" value="true">
