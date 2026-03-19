44583a6e790c1091509aeed090bf7b85862697f4
Tuck <kjtu228@uky.edu>
2026-03-19 12:06:45 -0400

    Update slam.js

--- src/js/slam.js
+++ src/js/slam.js
@@ -53,7 +53,12 @@ async function getCourseTools() {
 
 async function updateToolInstall(tool_id, confirmed = false) {
 	tool_toggle = document.getElementById("tool_select_" + tool_id);
-	tool_toggle.classList.add("updating");
+	// Get a NodeList of ALL descendant elements
+	const allDescendants = tool_toggle.querySelectorAll('*');
+	// You can iterate over the NodeList using the forEach method
+	allDescendants.forEach(element => {
+		console.log(element.tagName);
+	});
 	tool_container = document.getElementById('lti_tool_' + tool_id);
 	url = window.location.href.substring(0, document.location.href.lastIndexOf("/")) + '/exceptions.php?tool_id=';
 	url += tool_id + '&action=';
@@ -97,25 +102,20 @@ async function updateToolInstall(tool_id, confirmed = false) {
 					// display the message somehow
 				}
 				tool_toggle.disabled = false;
-				tool_toggle.classList.remove("updating");
 			} else {
 				if (Object.hasOwn(data, 'errors')) console.log(data['errors']);
 				if (data['action'] == 'add') {
 					tool_toggle.checked = false;
-					tool_toggle.classList.remove("updating");
 					tool_container.classList.remove("lti-tool-enabled");
 				} else {
 					tool_toggle.checked = true;
-					tool_toggle.classList.remove("updating");
 					tool_container.classList.add("lti-tool-enabled");
 				}
 				tool_toggle.disabled = false;
-				tool_toggle.classList.remove("updating");
 			}
 		}).catch(error => {
 			console.log(error);
 			tool_toggle.disabled = false;
-			tool_toggle.classList.remove("updating");
 		});
 }
 
